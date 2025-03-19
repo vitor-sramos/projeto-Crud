df0ab8c37397efe111756fd1e06ac2121718a24e
Vitor de Souza Ramos <vitor.sramos04@gmail.com>
2025-03-19 13:40:20 -0300

    Criando aruivo router.js para centralizar todas as rotas de todas as entidades, neste commit foi adicionado as rotas de usuários

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
 
-router.get("/usuarios", userController.getUser);
+router.get("/", userController.getUser);
 
 module.exports = router;
